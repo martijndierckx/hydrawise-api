72bf8c6cb1c4283132b6da7a87c03dfdc1503f60
martijndierckx <martijn@smard-media.be>
2020-05-03 23:37:38 +0200

    Adjusted test script to detect local/cloud test from number of args

--- test/test.js
+++ test/test.js
@@ -1,7 +1,12 @@
 const Hydrawise = require('../dist/Hydrawise').Hydrawise;
 
-const hydrawise = new Hydrawise({ type: 'LOCAL', host: process.argv[2], password: process.argv[3] });
-//const hydrawise = new Hydrawise({ type: 'CLOUD', key: process.argv[2] });
+//let hydrawise;
+if(process.argv.length <= 3) {
+  hydrawise = new Hydrawise({ type: 'CLOUD', key: process.argv[2] });
+}
+else {
+  hydrawise = new Hydrawise({ type: 'LOCAL', host: process.argv[2], password: process.argv[3] });
+}
 
 /* Get all zones -> run first zone -> get all zones -> stop first zone -> get all zones */
 hydrawise.getZones().then(function(data) {
