martijndierckx/hydrawise-api

commit eb0fe93c48f7ea500e45c693044228caa37ee445

Bugfixes

src/Hydrawise.ts
@@ -38,7 +38,7 @@ export class Hydrawise {
 	 */
 	constructor(options: HydrawiseConfiguration) {
 		this.type = options.type || HydrawiseConnectionType.CLOUD; // CLOUD or LOCAL 
-		this.url = options.host ? 'http://'+options.host+'/' : this.cloudUrl;
+		this.url = (this.type == HydrawiseConnectionType.LOCAL ? 'http://'+options.host+'/' : this.cloudUrl);
 		
 		// Local Auth
 		this.localAuthUsername = options.user || 'admin';
@@ -74,7 +74,7 @@ export class Hydrawise {
 			}
 			// API key auth for cloud binding
 			else {
-				options.qs.api_key = this.cloudAuthAPIkey;
+				options.params.api_key = this.cloudAuthAPIkey;
 			}
 
 			// Send request
@@ -231,7 +231,7 @@ export class Hydrawise {
 	 * @param {boolean} [onlyConfigured = true] - Only return zones/relays which have been configured
 	 * @return {Promise} A Promise which will be resolved when all zones have been retrieved
 	 */
-	public getZones(onlyConfigured: boolean = true): Promise<HydrawiseZone[]> {
+	public getZones(): Promise<HydrawiseZone[]> {
 		let that = this;
 	
 		// Get started
@@ -245,7 +245,7 @@ export class Hydrawise {
 				data.relays.map((z: any) => {
 					
 					// Only configured zones
-					if(onlyConfigured && z.type != 110) {
+					if(that.type == HydrawiseConnectionType.CLOUD || z.lastwaterepoch != 0){
 					
 						// Zone
 						let zone: any = {

dist/Hydrawise.d.ts
@@ -96,7 +96,7 @@ export declare class Hydrawise {
      * @param {boolean} [onlyConfigured = true] - Only return zones/relays which have been configured
      * @return {Promise} A Promise which will be resolved when all zones have been retrieved
      */
-    getZones(onlyConfigured?: boolean): Promise<HydrawiseZone[]>;
+    getZones(): Promise<HydrawiseZone[]>;
     /**
      * Retrieves all controllers known to the Hydrawise cloud
      * @return {Promise} A Promise which will be resolved when all controllers have been retrieved

dist/Hydrawise.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"Hydrawise.d.ts","sourceRoot":"","sources":["../src/Hydrawise.ts"],"names":[],"mappings":"AAAA;;;GAGG;AAEH,OAAO,EAAE,uBAAuB,EAAE,MAAM,2BAA2B,CAAC;AACpE,OAAO,EAAE,aAAa,EAAE,MAAM,iBAAiB,CAAC;AAChD,OAAO,EAAE,mBAAmB,EAAE,MAAM,uBAAuB,CAAC;AAI5D,UAAU,sBAAsB;IAC/B,IAAI,EAAG,uBAAuB,CAAC;IAC/B,IAAK,CAAC,EAAE,MAAM,CAAC;IACf,IAAK,CAAC,EAAE,MAAM,CAAC;IACf,QAAS,CAAC,EAAE,MAAM,CAAC;IACnB,GAAI,CAAC,EAAE,MAAM,CAAA;CACb;AAED,sEAAsE;AACtE,qBAAa,SAAS;IAErB,OAAO,CAAC,QAAQ,CAAC,QAAQ,CAA+C;IACjE,IAAI,EAAE,uBAAuB,CAAC;IAC9B,GAAG,EAAE,MAAM,CAAC;IACZ,iBAAiB,EAAE,MAAM,CAAC;IAC1B,iBAAiB,EAAE,MAAM,CAAC;IAC1B,eAAe,EAAE,MAAM,CAAC;IAE/B;;;;;;;;OAQG;gBACS,OAAO,EAAE,sBAAsB;IAY3C;;;;;OAKG;IACH,OAAO,CAAC,OAAO;IA0Cf;;;;;;;OAOG;IACI,WAAW,CAAC,MAAM,EAAE,MAAM,EAAE,WAAW,EAAE,MAAM,GAAG,aAAa,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAmCxG;;;;;;OAMG;IACI,eAAe,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAyBvE;;;;;OAKG;IACI,OAAO,CAAC,WAAW,EAAE,MAAM,GAAG,aAAa,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIpF;;;;OAIG;IACI,WAAW,CAAC,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAInD;;;;;OAKG;IACI,WAAW,CAAC,WAAW,EAAE,MAAM,GAAG,aAAa,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIxF;;;;OAIG;IACI,eAAe,CAAC,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIvD;;;;OAIG;IACI,QAAQ,CAAC,WAAW,EAAE,MAAM,GAAG,aAAa,GAAG,OAAO,CAAC,GAAG,CAAC;IAIlE;;;OAGG;IACI,YAAY,IAAI,OAAO,CAAC,GAAG,CAAC;IAInC;;;;OAIG;IACI,QAAQ,CAAC,cAAc,GAAE,OAAc,GAAG,OAAO,CAAC,aAAa,EAAE,CAAC;IA0DzE;;;OAGG;IACI,cAAc,IAAI,OAAO,CAAC,mBAAmB,EAAE,CAAC;IAuCvD;;;;OAIG;IACI,kBAAkB,CAAC,IAAI,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAWrD;;;;;OAKG;IACI,oBAAoB,CAAC,GAAG,GAAE,MAAW,EAAE,KAAK,GAAE,MAAc,GAAG,OAAO,CAAC,GAAG,CAAC;IAiBlF;;;;;;;OAOG;IACI,OAAO,CAAC,IAAI,EAAE,SAAS,EAAE,MAAM,GAAE,GAAQ,GAAG,OAAO,CAAC,GAAG,CAAC;IAU/D;;;;;OAKG;IACI,eAAe,CAAC,IAAI,GAAE,MAAsB,GAAG,OAAO,CAAC,GAAG,CAAC;IAIlE;;;;;OAKG;IACI,cAAc,CAAC,GAAG,GAAE,MAAW,EAAE,KAAK,GAAE,MAAc,GAAG,OAAO,CAAC,GAAG,CAAC;IAQ5E;;;;;OAKG;IACI,OAAO,CAAC,MAAM,GAAE,GAAQ,GAAG,OAAO,CAAC,GAAG,CAAC;CAG9C"}
+{"version":3,"file":"Hydrawise.d.ts","sourceRoot":"","sources":["../src/Hydrawise.ts"],"names":[],"mappings":"AAAA;;;GAGG;AAEH,OAAO,EAAE,uBAAuB,EAAE,MAAM,2BAA2B,CAAC;AACpE,OAAO,EAAE,aAAa,EAAE,MAAM,iBAAiB,CAAC;AAChD,OAAO,EAAE,mBAAmB,EAAE,MAAM,uBAAuB,CAAC;AAI5D,UAAU,sBAAsB;IAC/B,IAAI,EAAG,uBAAuB,CAAC;IAC/B,IAAK,CAAC,EAAE,MAAM,CAAC;IACf,IAAK,CAAC,EAAE,MAAM,CAAC;IACf,QAAS,CAAC,EAAE,MAAM,CAAC;IACnB,GAAI,CAAC,EAAE,MAAM,CAAA;CACb;AAED,sEAAsE;AACtE,qBAAa,SAAS;IAErB,OAAO,CAAC,QAAQ,CAAC,QAAQ,CAA+C;IACjE,IAAI,EAAE,uBAAuB,CAAC;IAC9B,GAAG,EAAE,MAAM,CAAC;IACZ,iBAAiB,EAAE,MAAM,CAAC;IAC1B,iBAAiB,EAAE,MAAM,CAAC;IAC1B,eAAe,EAAE,MAAM,CAAC;IAE/B;;;;;;;;OAQG;gBACS,OAAO,EAAE,sBAAsB;IAY3C;;;;;OAKG;IACH,OAAO,CAAC,OAAO;IA0Cf;;;;;;;OAOG;IACI,WAAW,CAAC,MAAM,EAAE,MAAM,EAAE,WAAW,EAAE,MAAM,GAAG,aAAa,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAmCxG;;;;;;OAMG;IACI,eAAe,CAAC,MAAM,EAAE,MAAM,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAyBvE;;;;;OAKG;IACI,OAAO,CAAC,WAAW,EAAE,MAAM,GAAG,aAAa,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIpF;;;;OAIG;IACI,WAAW,CAAC,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAInD;;;;;OAKG;IACI,WAAW,CAAC,WAAW,EAAE,MAAM,GAAG,aAAa,EAAE,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIxF;;;;OAIG;IACI,eAAe,CAAC,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIvD;;;;OAIG;IACI,QAAQ,CAAC,WAAW,EAAE,MAAM,GAAG,aAAa,GAAG,OAAO,CAAC,GAAG,CAAC;IAIlE;;;OAGG;IACI,YAAY,IAAI,OAAO,CAAC,GAAG,CAAC;IAInC;;;;OAIG;IACI,QAAQ,IAAI,OAAO,CAAC,aAAa,EAAE,CAAC;IA0D3C;;;OAGG;IACI,cAAc,IAAI,OAAO,CAAC,mBAAmB,EAAE,CAAC;IAuCvD;;;;OAIG;IACI,kBAAkB,CAAC,IAAI,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAWrD;;;;;OAKG;IACI,oBAAoB,CAAC,GAAG,GAAE,MAAW,EAAE,KAAK,GAAE,MAAc,GAAG,OAAO,CAAC,GAAG,CAAC;IAiBlF;;;;;;;OAOG;IACI,OAAO,CAAC,IAAI,EAAE,SAAS,EAAE,MAAM,GAAE,GAAQ,GAAG,OAAO,CAAC,GAAG,CAAC;IAU/D;;;;;OAKG;IACI,eAAe,CAAC,IAAI,GAAE,MAAsB,GAAG,OAAO,CAAC,GAAG,CAAC;IAIlE;;;;;OAKG;IACI,cAAc,CAAC,GAAG,GAAE,MAAW,EAAE,KAAK,GAAE,MAAc,GAAG,OAAO,CAAC,GAAG,CAAC;IAQ5E;;;;;OAKG;IACI,OAAO,CAAC,MAAM,GAAE,GAAQ,GAAG,OAAO,CAAC,GAAG,CAAC;CAG9C"}

dist/Hydrawise.js
@@ -26,7 +26,7 @@ class Hydrawise {
     constructor(options) {
         this.cloudUrl = 'https://app.hydrawise.com/api/v1/';
         this.type = options.type || HydrawiseConnectionType_1.HydrawiseConnectionType.CLOUD; // CLOUD or LOCAL 
-        this.url = options.host ? 'http://' + options.host + '/' : this.cloudUrl;
+        this.url = (this.type == HydrawiseConnectionType_1.HydrawiseConnectionType.LOCAL ? 'http://' + options.host + '/' : this.cloudUrl);
         // Local Auth
         this.localAuthUsername = options.user || 'admin';
         this.localAuthPassword = options.password || '';
@@ -57,7 +57,7 @@ class Hydrawise {
             }
             // API key auth for cloud binding
             else {
-                options.qs.api_key = this.cloudAuthAPIkey;
+                options.params.api_key = this.cloudAuthAPIkey;
             }
             // Send request
             axios_1.default(options).then((response) => {
@@ -191,7 +191,7 @@ class Hydrawise {
      * @param {boolean} [onlyConfigured = true] - Only return zones/relays which have been configured
      * @return {Promise} A Promise which will be resolved when all zones have been retrieved
      */
-    getZones(onlyConfigured = true) {
+    getZones() {
         let that = this;
         // Get started
         let promise = new Promise((resolve, reject) => {
@@ -201,7 +201,7 @@ class Hydrawise {
                 // Check every returned relay
                 data.relays.map((z) => {
                     // Only configured zones
-                    if (onlyConfigured && z.type != 110) {
+                    if (that.type == HydrawiseConnectionType_1.HydrawiseConnectionType.CLOUD || z.lastwaterepoch != 0) {
                         // Zone
                         let zone = {
                             apiBinding: that,

dist/Hydrawise.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"Hydrawise.js","sourceRoot":"","sources":["../src/Hydrawise.ts"],"names":[],"mappings":";AAAA;;;GAGG;;;;;AAEH,uEAAoE;AACpE,mDAAgD;AAChD,+DAA4D;AAC5D,2EAAwE;AACxE,kDAA0B;AAU1B,sEAAsE;AACtE,MAAa,SAAS;IASrB;;;;;;;;OAQG;IACH,YAAY,OAA+B;QAhB1B,aAAQ,GAAW,mCAAmC,CAAC;QAiBvE,IAAI,CAAC,IAAI,GAAG,OAAO,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,kBAAkB;QAC7E,IAAI,CAAC,GAAG,GAAG,OAAO,CAAC,IAAI,CAAC,CAAC,CAAC,SAAS,GAAC,OAAO,CAAC,IAAI,GAAC,GAAG,CAAC,CAAC,CAAC,IAAI,CAAC,QAAQ,CAAC;QAErE,aAAa;QACb,IAAI,CAAC,iBAAiB,GAAG,OAAO,CAAC,IAAI,IAAI,OAAO,CAAC;QACjD,IAAI,CAAC,iBAAiB,GAAG,OAAO,CAAC,QAAQ,IAAI,EAAE,CAAC;QAEhD,aAAa;QACb,IAAI,CAAC,eAAe,GAAG,OAAO,CAAC,GAAG,IAAI,EAAE,CAAC;IAC1C,CAAC;IAED;;;;;OAKG;IACK,OAAO,CAAC,OAAe,EAAE,EAAE,SAAc,EAAE;QAClD,IAAI,OAAO,GAAG,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAE7C,sBAAsB;YACtB,IAAI,OAAO,GAAQ;gBAClB,MAAM,EAAG,KAAK;gBACd,GAAG,EAAG,IAAI,CAAC,GAAG,GAAG,IAAI;gBACrB,MAAM,EAAG,MAAM;gBACf,IAAI,EAAG,IAAI;aACX,CAAC;YAEF,+BAA+B;YAC/B,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,EAAE;gBAC9C,IAAI,UAAU,GAAG,MAAM,CAAC,IAAI,CAAC,IAAI,CAAC,iBAAiB,GAAG,GAAG,GAAG,IAAI,CAAC,iBAAiB,CAAC,CAAC;gBACpF,OAAO,CAAC,OAAO,GAAG;oBACjB,eAAe,EAAE,QAAQ,GAAE,UAAU,CAAC,QAAQ,CAAC,QAAQ,CAAC;iBACxD,CAAC;aACF;YACD,iCAAiC;iBAC5B;gBACJ,OAAO,CAAC,EAAE,CAAC,OAAO,GAAG,IAAI,CAAC,eAAe,CAAC;aAC1C;YAED,eAAe;YACf,eAAK,CAAC,OAAO,CAAC,CAAC,IAAI,CAAC,CAAC,QAAa,EAAE,EAAE;gBAErC,kBAAkB;gBAClB,IAAG,QAAQ,CAAC,IAAI,CAAC,WAAW,IAAI,OAAO,EAAE;oBACxC,MAAM,CAAC,IAAI,qDAAyB,CAAC,QAAQ,CAAC,IAAI,CAAC,OAAO,CAAC,CAAC,CAAC;iBAC7D;gBAED,OAAO,CAAC,QAAQ,CAAC,IAAI,CAAC,CAAC;YAExB,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAU,EAAE,EAAE;gBACvB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,iBAAiB;QACjB,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;;;;;OAOG;IACI,WAAW,CAAC,MAAc,EAAE,WAAmC,EAAE,QAAiB;QACxF,IAAI,IAAI,GAAa,IAAI,CAAC;QAE1B,cAAc;QACd,IAAI,OAAO,GAAG,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAC7C,IAAI,IAAI,GAAQ;gBACf,SAAS,EAAG,GAAG;gBACf,MAAM,EAAE,MAAM;aACd,CAAC;YAEF,qCAAqC;YACrC,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,EAAE;gBAC9C,IAAI,CAAC,KAAK,GAAG,OAAO,WAAW,IAAI,QAAQ,CAAC,CAAC,CAAC,WAAW,CAAC,IAAI,CAAC,CAAC,CAAC,WAAW,CAAA,CAAC,0EAA0E;aACvJ;YACD,iCAAiC;iBAC5B;gBACJ,IAAI,CAAC,QAAQ,GAAG,OAAO,WAAW,IAAI,QAAQ,CAAC,CAAC,CAAC,WAAW,CAAC,OAAO,CAAC,CAAC,CAAC,WAAW,CAAA,CAAC,0EAA0E;aAC7J;YAED,mBAAmB;YACnB,IAAG,QAAQ,KAAK,SAAS,EAAE;gBAC1B,IAAI,CAAC,MAAM,GAAG,QAAQ,CAAC;aACvB;YAED,kBAAkB;YAClB,IAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,IAAI,CAAC,CAAC,IAAS,EAAE,EAAE;gBACrC,OAAO,CAAC,IAAI,CAAC,CAAC;YACf,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;;;;OAMG;IACI,eAAe,CAAC,MAAc,EAAE,QAAiB;QACvD,IAAI,IAAI,GAAG,IAAI,CAAC;QAEhB,cAAc;QACd,IAAI,OAAO,GAAG,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAC7C,IAAI,IAAI,GAAQ;gBACf,SAAS,EAAG,GAAG;gBACf,MAAM,EAAE,MAAM;aACd,CAAA;YAED,mBAAmB;YACnB,IAAG,QAAQ,KAAK,SAAS,EAAE;gBAC1B,IAAI,CAAC,MAAM,GAAG,QAAQ,CAAC;aACvB;YAED,IAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,IAAI,CAAC,IAAI,CAAC,EAAE;gBAC9B,OAAO,CAAC,IAAI,CAAC,CAAC;YACf,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;;;OAKG;IACI,OAAO,CAAC,WAAmC,EAAE,QAAiB;QACpE,OAAO,IAAI,CAAC,WAAW,CAAC,KAAK,EAAE,WAAW,EAAE,QAAQ,CAAC,CAAC;IACvD,CAAC;IAED;;;;OAIG;IACI,WAAW,CAAC,QAAiB;QACnC,OAAO,IAAI,CAAC,eAAe,CAAC,QAAQ,EAAE,QAAQ,CAAC,CAAC;IACjD,CAAC;IAED;;;;;OAKG;IACI,WAAW,CAAC,WAAmC,EAAE,QAAiB;QACxE,OAAO,IAAI,CAAC,WAAW,CAAC,SAAS,EAAE,WAAW,EAAE,QAAQ,CAAC,CAAC;IAC3D,CAAC;IAED;;;;OAIG;IACI,eAAe,CAAC,QAAiB;QACvC,OAAO,IAAI,CAAC,eAAe,CAAC,YAAY,EAAE,QAAQ,CAAC,CAAC;IACrD,CAAC;IAED;;;;OAIG;IACI,QAAQ,CAAC,WAAmC;QAClD,OAAO,IAAI,CAAC,WAAW,CAAC,MAAM,EAAE,WAAW,CAAC,CAAC;IAC9C,CAAC;IAED;;;OAGG;IACI,YAAY;QAClB,OAAO,IAAI,CAAC,eAAe,CAAC,SAAS,CAAC,CAAC;IACxC,CAAC;IAED;;;;OAIG;IACI,QAAQ,CAAC,iBAA0B,IAAI;QAC7C,IAAI,IAAI,GAAG,IAAI,CAAC;QAEhB,cAAc;QACd,IAAI,OAAO,GAA6B,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAEvE,aAAa;YACb,IAAI,CAAC,oBAAoB,EAAE,CAAC,IAAI,CAAC,CAAC,IAAS,EAAE,EAAE;gBAC9C,IAAI,KAAK,GAAmB,EAAE,CAAC;gBAE/B,6BAA6B;gBAC7B,IAAI,CAAC,MAAM,CAAC,GAAG,CAAC,CAAC,CAAM,EAAE,EAAE;oBAE1B,wBAAwB;oBACxB,IAAG,cAAc,IAAI,CAAC,CAAC,IAAI,IAAI,GAAG,EAAE;wBAEnC,OAAO;wBACP,IAAI,IAAI,GAAQ;4BACf,UAAU,EAAE,IAAI;4BAChB,OAAO,EAAE,CAAC,CAAC,QAAQ;4BACnB,IAAI,EAAE,CAAC,CAAC,KAAK;4BACb,IAAI,EAAE,CAAC,CAAC,IAAI;4BACZ,SAAS,EAAE,IAAI,IAAI,CAAC,CAAC,IAAI,CAAC,IAAI,GAAG,CAAC,CAAC,IAAI,CAAC,GAAG,IAAI,CAAC;4BAChD,eAAe,EAAE,CAAC,CAAC,GAAG,IAAI,CAAC,CAAC,WAAW;4BACvC,WAAW,EAAE,CAAC,CAAC,SAAS,KAAK,SAAS,IAAI,CAAC,CAAC,SAAS,IAAI,CAAC;4BAC1D,SAAS,EAAE,KAAK;4BAChB,oBAAoB,EAAE,CAAC;yBACvB,CAAC;wBAEF,4CAA4C;wBAC5C,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,EAAE;4BAC9C,IAAI,CAAC,kBAAkB,GAAG,CAAC,CAAC,aAAa,GAAG,EAAE,CAAC;yBAC/C;wBAED,WAAW;wBACX,IAAG,IAAI,CAAC,OAAO,KAAK,SAAS,EAAE;4BAC9B,IAAI,WAAW,GAAG,IAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAM,EAAE,EAAE;gCAC9C,OAAO,CAAC,CAAC,QAAQ,IAAI,CAAC,CAAC,QAAQ,CAAC;4BACjC,CAAC,CAAC,CAAC;4BACH,IAAG,WAAW,IAAI,SAAS,IAAI,WAAW,IAAI,IAAI,EAAE;gCACnD,IAAI,CAAC,SAAS,GAAG,IAAI,CAAC;gCACtB,IAAI,CAAC,oBAAoB,GAAG,WAAW,CAAC,SAAS,CAAC;6BAClD;yBACD;wBAED,KAAK,CAAC,IAAI,CAAC,IAAI,6BAAa,CAAC,IAAI,CAAC,CAAC,CAAC;qBACpC;gBACF,CAAC,CAAC,CAAC;gBAEH,OAAO,CAAC,KAAK,CAAC,CAAC;YAChB,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;OAGG;IACI,cAAc;QACpB,IAAI,IAAI,GAAG,IAAI,CAAC;QAEhB,cAAc;QACd,IAAI,OAAO,GAAmC,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAE7E,kBAAkB;YAClB,IAAI,CAAC,kBAAkB,CAAC,aAAa,CAAC,CAAC,IAAI,CAAC,IAAI,CAAC,EAAE;gBAClD,IAAI,WAAW,GAA0B,EAAE,CAAC;gBAE5C,6BAA6B;gBAC7B,IAAI,CAAC,WAAW,CAAC,GAAG,CAAC,CAAC,CAAM,EAAE,EAAE;oBAE/B,OAAO;oBACP,IAAI,UAAU,GAAQ;wBACrB,EAAE,EAAE,CAAC,CAAC,aAAa;wBACnB,IAAI,EAAE,CAAC,CAAC,IAAI;wBACZ,YAAY,EAAE,CAAC,CAAC,aAAa;wBAC7B,oBAAoB,EAAE,IAAI,IAAI,CAAC,CAAC,CAAC,YAAY,GAAG,IAAI,CAAC;wBACrD,MAAM,EAAE,CAAC,CAAC,MAAM;qBAChB,CAAC;oBAEF,WAAW,CAAC,IAAI,CAAC,IAAI,yCAAmB,CAAC,UAAU,CAAC,CAAC,CAAC;gBACvD,CAAC,CAAC,CAAC;gBAEH,OAAO,CAAC,WAAW,CAAC,CAAC;YACtB,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QAEJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAID,qCAAqC;IAErC;;;;OAIG;IACI,kBAAkB,CAAC,IAAY;QACrC,iBAAiB;QACjB,IAAI,IAAI,CAAC,IAAI,IAAK,iDAAuB,CAAC,KAAK,EAAE;YAChD,OAAO,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;gBACtC,MAAM,CAAC,IAAI,qDAAyB,CAAC,+CAA+C,CAAC,CAAC,CAAC;YACxF,CAAC,CAAC,CAAC;SACH;QAED,OAAO,IAAI,CAAC,OAAO,CAAC,qBAAqB,EAAE,EAAE,IAAI,EAAE,IAAI,EAAE,CAAC,CAAC;IAC5D,CAAC;IAED;;;;;OAKG;IACI,oBAAoB,CAAC,MAAc,EAAE,EAAE,QAAgB,KAAK;QAClE,IAAI,GAAG,GAAG,CAAC,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,CAAC,oBAAoB,CAAC,CAAC,CAAC,oBAAoB,CAAC,CAAC;QAErG,OAAO,IAAI,CAAC,OAAO,CAAC,GAAG,EAAE,EAAE,GAAG,EAAE,KAAK,EAAE,CAAC,CAAC;IAC1C,CAAC;IAED;;;;;;;;;OASG;IAEH;;;;;;;OAOG;IACI,OAAO,CAAkB,SAAc,EAAE;QAC/C,IAAI,GAAG,GAAW,CAAC,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,CAAC,qBAAqB,CAAC,CAAC,CAAC,aAAa,CAAC,CAAC;QAEvG,OAAO,IAAI,CAAC,OAAO,CAAC,GAAG,EAAE,MAAM,CAAC,CAAC;IAClC,CAAC;IAID,iFAAiF;IAEjF;;;;;OAKG;IACI,eAAe,CAAC,OAAe,aAAa;QAClD,OAAO,IAAI,CAAC,kBAAkB,CAAC,IAAI,CAAC,CAAC;IACtC,CAAC;IAED;;;;;OAKG;IACI,cAAc,CAAC,MAAc,EAAE,EAAE,QAAgB,KAAK;QAC5D,OAAO,IAAI,CAAC,oBAAoB,CAAC,GAAG,EAAE,KAAK,CAAC,CAAC;IAC9C,CAAC;IAED;;OAEG;IAEH;;;;;OAKG;IACI,OAAO,CAAC,SAAc,EAAE;QAC9B,OAAO,IAAI,CAAC,OAAO,CAAC,MAAM,CAAC,CAAC;IAC7B,CAAC;CACD;AApZD,8BAoZC"}
+{"version":3,"file":"Hydrawise.js","sourceRoot":"","sources":["../src/Hydrawise.ts"],"names":[],"mappings":";AAAA;;;GAGG;;;;;AAEH,uEAAoE;AACpE,mDAAgD;AAChD,+DAA4D;AAC5D,2EAAwE;AACxE,kDAA0B;AAU1B,sEAAsE;AACtE,MAAa,SAAS;IASrB;;;;;;;;OAQG;IACH,YAAY,OAA+B;QAhB1B,aAAQ,GAAW,mCAAmC,CAAC;QAiBvE,IAAI,CAAC,IAAI,GAAG,OAAO,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,kBAAkB;QAC7E,IAAI,CAAC,GAAG,GAAG,CAAC,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,CAAC,SAAS,GAAC,OAAO,CAAC,IAAI,GAAC,GAAG,CAAC,CAAC,CAAC,IAAI,CAAC,QAAQ,CAAC,CAAC;QAErG,aAAa;QACb,IAAI,CAAC,iBAAiB,GAAG,OAAO,CAAC,IAAI,IAAI,OAAO,CAAC;QACjD,IAAI,CAAC,iBAAiB,GAAG,OAAO,CAAC,QAAQ,IAAI,EAAE,CAAC;QAEhD,aAAa;QACb,IAAI,CAAC,eAAe,GAAG,OAAO,CAAC,GAAG,IAAI,EAAE,CAAC;IAC1C,CAAC;IAED;;;;;OAKG;IACK,OAAO,CAAC,OAAe,EAAE,EAAE,SAAc,EAAE;QAClD,IAAI,OAAO,GAAG,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAE7C,sBAAsB;YACtB,IAAI,OAAO,GAAQ;gBAClB,MAAM,EAAG,KAAK;gBACd,GAAG,EAAG,IAAI,CAAC,GAAG,GAAG,IAAI;gBACrB,MAAM,EAAG,MAAM;gBACf,IAAI,EAAG,IAAI;aACX,CAAC;YAEF,+BAA+B;YAC/B,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,EAAE;gBAC9C,IAAI,UAAU,GAAG,MAAM,CAAC,IAAI,CAAC,IAAI,CAAC,iBAAiB,GAAG,GAAG,GAAG,IAAI,CAAC,iBAAiB,CAAC,CAAC;gBACpF,OAAO,CAAC,OAAO,GAAG;oBACjB,eAAe,EAAE,QAAQ,GAAE,UAAU,CAAC,QAAQ,CAAC,QAAQ,CAAC;iBACxD,CAAC;aACF;YACD,iCAAiC;iBAC5B;gBACJ,OAAO,CAAC,MAAM,CAAC,OAAO,GAAG,IAAI,CAAC,eAAe,CAAC;aAC9C;YAED,eAAe;YACf,eAAK,CAAC,OAAO,CAAC,CAAC,IAAI,CAAC,CAAC,QAAa,EAAE,EAAE;gBAErC,kBAAkB;gBAClB,IAAG,QAAQ,CAAC,IAAI,CAAC,WAAW,IAAI,OAAO,EAAE;oBACxC,MAAM,CAAC,IAAI,qDAAyB,CAAC,QAAQ,CAAC,IAAI,CAAC,OAAO,CAAC,CAAC,CAAC;iBAC7D;gBAED,OAAO,CAAC,QAAQ,CAAC,IAAI,CAAC,CAAC;YAExB,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAU,EAAE,EAAE;gBACvB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,iBAAiB;QACjB,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;;;;;OAOG;IACI,WAAW,CAAC,MAAc,EAAE,WAAmC,EAAE,QAAiB;QACxF,IAAI,IAAI,GAAa,IAAI,CAAC;QAE1B,cAAc;QACd,IAAI,OAAO,GAAG,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAC7C,IAAI,IAAI,GAAQ;gBACf,SAAS,EAAG,GAAG;gBACf,MAAM,EAAE,MAAM;aACd,CAAC;YAEF,qCAAqC;YACrC,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,EAAE;gBAC9C,IAAI,CAAC,KAAK,GAAG,OAAO,WAAW,IAAI,QAAQ,CAAC,CAAC,CAAC,WAAW,CAAC,IAAI,CAAC,CAAC,CAAC,WAAW,CAAA,CAAC,0EAA0E;aACvJ;YACD,iCAAiC;iBAC5B;gBACJ,IAAI,CAAC,QAAQ,GAAG,OAAO,WAAW,IAAI,QAAQ,CAAC,CAAC,CAAC,WAAW,CAAC,OAAO,CAAC,CAAC,CAAC,WAAW,CAAA,CAAC,0EAA0E;aAC7J;YAED,mBAAmB;YACnB,IAAG,QAAQ,KAAK,SAAS,EAAE;gBAC1B,IAAI,CAAC,MAAM,GAAG,QAAQ,CAAC;aACvB;YAED,kBAAkB;YAClB,IAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,IAAI,CAAC,CAAC,IAAS,EAAE,EAAE;gBACrC,OAAO,CAAC,IAAI,CAAC,CAAC;YACf,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;;;;OAMG;IACI,eAAe,CAAC,MAAc,EAAE,QAAiB;QACvD,IAAI,IAAI,GAAG,IAAI,CAAC;QAEhB,cAAc;QACd,IAAI,OAAO,GAAG,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAC7C,IAAI,IAAI,GAAQ;gBACf,SAAS,EAAG,GAAG;gBACf,MAAM,EAAE,MAAM;aACd,CAAA;YAED,mBAAmB;YACnB,IAAG,QAAQ,KAAK,SAAS,EAAE;gBAC1B,IAAI,CAAC,MAAM,GAAG,QAAQ,CAAC;aACvB;YAED,IAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,IAAI,CAAC,IAAI,CAAC,EAAE;gBAC9B,OAAO,CAAC,IAAI,CAAC,CAAC;YACf,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;;;OAKG;IACI,OAAO,CAAC,WAAmC,EAAE,QAAiB;QACpE,OAAO,IAAI,CAAC,WAAW,CAAC,KAAK,EAAE,WAAW,EAAE,QAAQ,CAAC,CAAC;IACvD,CAAC;IAED;;;;OAIG;IACI,WAAW,CAAC,QAAiB;QACnC,OAAO,IAAI,CAAC,eAAe,CAAC,QAAQ,EAAE,QAAQ,CAAC,CAAC;IACjD,CAAC;IAED;;;;;OAKG;IACI,WAAW,CAAC,WAAmC,EAAE,QAAiB;QACxE,OAAO,IAAI,CAAC,WAAW,CAAC,SAAS,EAAE,WAAW,EAAE,QAAQ,CAAC,CAAC;IAC3D,CAAC;IAED;;;;OAIG;IACI,eAAe,CAAC,QAAiB;QACvC,OAAO,IAAI,CAAC,eAAe,CAAC,YAAY,EAAE,QAAQ,CAAC,CAAC;IACrD,CAAC;IAED;;;;OAIG;IACI,QAAQ,CAAC,WAAmC;QAClD,OAAO,IAAI,CAAC,WAAW,CAAC,MAAM,EAAE,WAAW,CAAC,CAAC;IAC9C,CAAC;IAED;;;OAGG;IACI,YAAY;QAClB,OAAO,IAAI,CAAC,eAAe,CAAC,SAAS,CAAC,CAAC;IACxC,CAAC;IAED;;;;OAIG;IACI,QAAQ;QACd,IAAI,IAAI,GAAG,IAAI,CAAC;QAEhB,cAAc;QACd,IAAI,OAAO,GAA6B,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAEvE,aAAa;YACb,IAAI,CAAC,oBAAoB,EAAE,CAAC,IAAI,CAAC,CAAC,IAAS,EAAE,EAAE;gBAC9C,IAAI,KAAK,GAAmB,EAAE,CAAC;gBAE/B,6BAA6B;gBAC7B,IAAI,CAAC,MAAM,CAAC,GAAG,CAAC,CAAC,CAAM,EAAE,EAAE;oBAE1B,wBAAwB;oBACxB,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,IAAI,CAAC,CAAC,cAAc,IAAI,CAAC,EAAC;wBAEtE,OAAO;wBACP,IAAI,IAAI,GAAQ;4BACf,UAAU,EAAE,IAAI;4BAChB,OAAO,EAAE,CAAC,CAAC,QAAQ;4BACnB,IAAI,EAAE,CAAC,CAAC,KAAK;4BACb,IAAI,EAAE,CAAC,CAAC,IAAI;4BACZ,SAAS,EAAE,IAAI,IAAI,CAAC,CAAC,IAAI,CAAC,IAAI,GAAG,CAAC,CAAC,IAAI,CAAC,GAAG,IAAI,CAAC;4BAChD,eAAe,EAAE,CAAC,CAAC,GAAG,IAAI,CAAC,CAAC,WAAW;4BACvC,WAAW,EAAE,CAAC,CAAC,SAAS,KAAK,SAAS,IAAI,CAAC,CAAC,SAAS,IAAI,CAAC;4BAC1D,SAAS,EAAE,KAAK;4BAChB,oBAAoB,EAAE,CAAC;yBACvB,CAAC;wBAEF,4CAA4C;wBAC5C,IAAG,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,EAAE;4BAC9C,IAAI,CAAC,kBAAkB,GAAG,CAAC,CAAC,aAAa,GAAG,EAAE,CAAC;yBAC/C;wBAED,WAAW;wBACX,IAAG,IAAI,CAAC,OAAO,KAAK,SAAS,EAAE;4BAC9B,IAAI,WAAW,GAAG,IAAI,CAAC,OAAO,CAAC,IAAI,CAAC,CAAC,CAAM,EAAE,EAAE;gCAC9C,OAAO,CAAC,CAAC,QAAQ,IAAI,CAAC,CAAC,QAAQ,CAAC;4BACjC,CAAC,CAAC,CAAC;4BACH,IAAG,WAAW,IAAI,SAAS,IAAI,WAAW,IAAI,IAAI,EAAE;gCACnD,IAAI,CAAC,SAAS,GAAG,IAAI,CAAC;gCACtB,IAAI,CAAC,oBAAoB,GAAG,WAAW,CAAC,SAAS,CAAC;6BAClD;yBACD;wBAED,KAAK,CAAC,IAAI,CAAC,IAAI,6BAAa,CAAC,IAAI,CAAC,CAAC,CAAC;qBACpC;gBACF,CAAC,CAAC,CAAC;gBAEH,OAAO,CAAC,KAAK,CAAC,CAAC;YAChB,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QACJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAED;;;OAGG;IACI,cAAc;QACpB,IAAI,IAAI,GAAG,IAAI,CAAC;QAEhB,cAAc;QACd,IAAI,OAAO,GAAmC,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;YAE7E,kBAAkB;YAClB,IAAI,CAAC,kBAAkB,CAAC,aAAa,CAAC,CAAC,IAAI,CAAC,IAAI,CAAC,EAAE;gBAClD,IAAI,WAAW,GAA0B,EAAE,CAAC;gBAE5C,6BAA6B;gBAC7B,IAAI,CAAC,WAAW,CAAC,GAAG,CAAC,CAAC,CAAM,EAAE,EAAE;oBAE/B,OAAO;oBACP,IAAI,UAAU,GAAQ;wBACrB,EAAE,EAAE,CAAC,CAAC,aAAa;wBACnB,IAAI,EAAE,CAAC,CAAC,IAAI;wBACZ,YAAY,EAAE,CAAC,CAAC,aAAa;wBAC7B,oBAAoB,EAAE,IAAI,IAAI,CAAC,CAAC,CAAC,YAAY,GAAG,IAAI,CAAC;wBACrD,MAAM,EAAE,CAAC,CAAC,MAAM;qBAChB,CAAC;oBAEF,WAAW,CAAC,IAAI,CAAC,IAAI,yCAAmB,CAAC,UAAU,CAAC,CAAC,CAAC;gBACvD,CAAC,CAAC,CAAC;gBAEH,OAAO,CAAC,WAAW,CAAC,CAAC;YACtB,CAAC,CAAC,CAAC,KAAK,CAAC,CAAC,GAAG,EAAE,EAAE;gBAChB,MAAM,CAAC,GAAG,CAAC,CAAC;YACb,CAAC,CAAC,CAAC;QAEJ,CAAC,CAAC,CAAC;QAEH,OAAO,OAAO,CAAC;IAChB,CAAC;IAID,qCAAqC;IAErC;;;;OAIG;IACI,kBAAkB,CAAC,IAAY;QACrC,iBAAiB;QACjB,IAAI,IAAI,CAAC,IAAI,IAAK,iDAAuB,CAAC,KAAK,EAAE;YAChD,OAAO,IAAI,OAAO,CAAC,CAAC,OAAO,EAAE,MAAM,EAAE,EAAE;gBACtC,MAAM,CAAC,IAAI,qDAAyB,CAAC,+CAA+C,CAAC,CAAC,CAAC;YACxF,CAAC,CAAC,CAAC;SACH;QAED,OAAO,IAAI,CAAC,OAAO,CAAC,qBAAqB,EAAE,EAAE,IAAI,EAAE,IAAI,EAAE,CAAC,CAAC;IAC5D,CAAC;IAED;;;;;OAKG;IACI,oBAAoB,CAAC,MAAc,EAAE,EAAE,QAAgB,KAAK;QAClE,IAAI,GAAG,GAAG,CAAC,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,CAAC,oBAAoB,CAAC,CAAC,CAAC,oBAAoB,CAAC,CAAC;QAErG,OAAO,IAAI,CAAC,OAAO,CAAC,GAAG,EAAE,EAAE,GAAG,EAAE,KAAK,EAAE,CAAC,CAAC;IAC1C,CAAC;IAED;;;;;;;;;OASG;IAEH;;;;;;;OAOG;IACI,OAAO,CAAkB,SAAc,EAAE;QAC/C,IAAI,GAAG,GAAW,CAAC,IAAI,CAAC,IAAI,IAAI,iDAAuB,CAAC,KAAK,CAAC,CAAC,CAAC,qBAAqB,CAAC,CAAC,CAAC,aAAa,CAAC,CAAC;QAEvG,OAAO,IAAI,CAAC,OAAO,CAAC,GAAG,EAAE,MAAM,CAAC,CAAC;IAClC,CAAC;IAID,iFAAiF;IAEjF;;;;;OAKG;IACI,eAAe,CAAC,OAAe,aAAa;QAClD,OAAO,IAAI,CAAC,kBAAkB,CAAC,IAAI,CAAC,CAAC;IACtC,CAAC;IAED;;;;;OAKG;IACI,cAAc,CAAC,MAAc,EAAE,EAAE,QAAgB,KAAK;QAC5D,OAAO,IAAI,CAAC,oBAAoB,CAAC,GAAG,EAAE,KAAK,CAAC,CAAC;IAC9C,CAAC;IAED;;OAEG;IAEH;;;;;OAKG;IACI,OAAO,CAAC,SAAc,EAAE;QAC9B,OAAO,IAAI,CAAC,OAAO,CAAC,MAAM,CAAC,CAAC;IAC7B,CAAC;CACD;AApZD,8BAoZC"}

dist/HydrawiseZone.d.ts
@@ -32,7 +32,7 @@ export declare class HydrawiseZone {
      * @param {number} [duration] - How long should the command be executed
      * @return {Promise} A Promise which will be resolved when the command has been executed.
      */
-    run(duration: number): Promise<any>;
+    run(duration?: number): Promise<any>;
     /**
      * Sends the stop command to the zone/relay
      * @return {Promise} A Promise which will be resolved when the command has been executed.
@@ -43,6 +43,6 @@ export declare class HydrawiseZone {
      * @param {number} [duration] - How long should the command be executed
      * @return {Promise} A Promise which will be resolved when the command has been executed.
      */
-    suspend(duration: number): Promise<any>;
+    suspend(duration?: number): Promise<any>;
 }
 //# sourceMappingURL=HydrawiseZone.d.ts.map

dist/HydrawiseZone.d.ts.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"HydrawiseZone.d.ts","sourceRoot":"","sources":["../src/HydrawiseZone.ts"],"names":[],"mappings":"AAAA;;GAEG;AAEH,OAAO,EAAE,SAAS,EAAE,MAAM,aAAa,CAAC;AAExC,0CAA0C;AAC1C,qBAAa,aAAa;IAElB,UAAU,EAAE,SAAS,CAAC;IACtB,OAAO,EAAE,MAAM,CAAC;IAChB,IAAI,EAAE,MAAM,CAAC;IACb,IAAI,EAAE,MAAM,CAAC;IACb,SAAS,EAAE,IAAI,CAAC;IAChB,eAAe,EAAE,MAAM,CAAC;IACxB,WAAW,EAAE,OAAO,CAAC;IACrB,SAAS,EAAE,OAAO,CAAC;IACnB,oBAAoB,EAAE,MAAM,CAAC;IAEpC;;;;;;;;;;;;OAYG;gBACS,OAAO,EAAE,GAAG;IAYxB;;;;OAIG;IACH,GAAG,CAAC,QAAQ,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAInC;;;OAGG;IACH,IAAI,IAAI,OAAO,CAAC,GAAG,CAAC;IAIpB;;;;OAIG;IACH,OAAO,CAAC,QAAQ,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;CAGvC"}
+{"version":3,"file":"HydrawiseZone.d.ts","sourceRoot":"","sources":["../src/HydrawiseZone.ts"],"names":[],"mappings":"AAAA;;GAEG;AAEH,OAAO,EAAE,SAAS,EAAE,MAAM,aAAa,CAAC;AAExC,0CAA0C;AAC1C,qBAAa,aAAa;IAElB,UAAU,EAAE,SAAS,CAAC;IACtB,OAAO,EAAE,MAAM,CAAC;IAChB,IAAI,EAAE,MAAM,CAAC;IACb,IAAI,EAAE,MAAM,CAAC;IACb,SAAS,EAAE,IAAI,CAAC;IAChB,eAAe,EAAE,MAAM,CAAC;IACxB,WAAW,EAAE,OAAO,CAAC;IACrB,SAAS,EAAE,OAAO,CAAC;IACnB,oBAAoB,EAAE,MAAM,CAAC;IAEpC;;;;;;;;;;;;OAYG;gBACS,OAAO,EAAE,GAAG;IAYxB;;;;OAIG;IACH,GAAG,CAAC,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;IAIpC;;;OAGG;IACH,IAAI,IAAI,OAAO,CAAC,GAAG,CAAC;IAIpB;;;;OAIG;IACH,OAAO,CAAC,QAAQ,CAAC,EAAE,MAAM,GAAG,OAAO,CAAC,GAAG,CAAC;CAGxC"}

commit 1fd66e0ad2e9342fe016457c776cc0ce90490911

fix: LOCAL zone filter — drop type=110, not lastwaterepoch=0
The LOCAL parseZones filter was using `lastwaterepoch === 0` as a
heuristic for "unconfigured relay slot". That's unsafe: a real,
named, configured zone that has simply never been watered also has
lastwaterepoch=0, so it was silently dropped — resulting in an empty
zone list and zero accessories in homebridge-hydrawise after a fresh
install or a controller reset.

Reverts to the pre-2020 predicate `type === 110`, which is the
controller's own marker for an empty relay slot (commit eb0fe93
mistakenly replaced it). CLOUD responses are pre-filtered by the
API itself, so no filter is applied there.

- Adds `RelayRow.type?: number` to the typed local response.
- Updates LOCAL parseZones fixture: configured zones use type=9, the
  unconfigured one uses type=110; Front Lawn gets lastwaterepoch=0
  as a regression case proving it stays included now.
- Adds an explicit regression test that asserts a configured relay
  with lastwaterepoch=0 is returned.

Bumps to 2.0.1.

Co-Authored-By: Claude Opus 4.7 <noreply@anthropic.com>

src/Hydrawise.ts
@@ -212,7 +212,11 @@ export class Hydrawise {
     const running: RunningRow[] = (data as LocalStatusResponse).running ?? [];
 
     for (const z of data.relays) {
-      if (this.type === HydrawiseConnectionType.LOCAL && z.lastwaterepoch === 0) {
+      // LOCAL responses include all relay slots; unconfigured ones are tagged `type: 110`.
+      // The pre-2020 filter (`type != 110`) was correct; it was replaced with a
+      // `lastwaterepoch !== 0` check that dropped real zones that had never run.
+      // CLOUD responses are pre-filtered to configured zones only — no filter needed.
+      if (this.type === HydrawiseConnectionType.LOCAL && z.type === 110) {
         continue;
       }
       const init = this.zoneFromRow(z, data.time, running, controller);

src/types.ts
@@ -24,6 +24,8 @@ export interface RelayRow {
   suspended?: 0 | 1;
   normalRuntime?: number;
   lastwaterepoch?: number;
+  /** LOCAL-only: relay slot type. `110` indicates an unconfigured/empty slot. */
+  type?: number;
 }
 
 export interface RunningRow {

test/parseLocalStatus.test.ts
@@ -7,12 +7,22 @@ import localRunningFixture from './fixtures/local-get-sched-with-running.json';
 describe('LOCAL getZones()', () => {
   afterEach(() => restoreFetchMock());
 
-  it('excludes relays with lastwaterepoch == 0', async () => {
+  it('excludes relays with type == 110 (unconfigured slots)', async () => {
     setupFetchMock(localFixture);
     const h = new Hydrawise({ type: HydrawiseConnectionType.LOCAL, host: 'h', password: 'p' });
     const zones = await h.getZones();
     expect(zones).toHaveLength(2);
     expect(zones.map((z) => z.name)).toEqual(['Front Lawn', 'Back Lawn']);
+  });
+
+  it('regression: includes configured relays whose lastwaterepoch is 0 (never watered)', async () => {
+    // Front Lawn in the fixture has type=9, lastwaterepoch=0 — must be included.
+    // Pre-fix behavior dropped it via the unsafe lastwaterepoch === 0 heuristic.
+    setupFetchMock(localFixture);
+    const h = new Hydrawise({ type: HydrawiseConnectionType.LOCAL, host: 'h', password: 'p' });
+    const zones = await h.getZones();
+    const front = zones.find((z) => z.name === 'Front Lawn');
+    expect(front).toBeDefined();
   });
 
   it('maps relay_id → relayID and relay → zone', async () => {