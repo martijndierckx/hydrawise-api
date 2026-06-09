9077403e1829406a651b11be2950e6d213a97c10
Martijn Dierckx <martijn@smard.be>
2026-06-09 09:07:57 +0200

    cleanup

--- dist/HydrawiseCommandException.d.ts
+++
@@ -1,6 +0,0 @@
-/** Specific error thrown by the Hydrawise API binding when the server replies with messageType === 'error'. */
-export declare class HydrawiseCommandException extends Error {
-    readonly date: Date;
-    constructor(message: string);
-}
-//# sourceMappingURL=HydrawiseCommandException.d.ts.map
--- dist/http.d.ts
+++
@@ -1,6 +0,0 @@
-import type { HydrawiseConfig } from './types';
-export interface HttpClient {
-    get<T>(path: string, params?: Record<string, string | number | undefined>): Promise<T>;
-}
-export declare function createHttpClient(config: HydrawiseConfig): HttpClient;
-//# sourceMappingURL=http.d.ts.map
